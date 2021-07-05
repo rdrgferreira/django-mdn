81fa8d59874edc04e1352b87a8114f1ee267e34b
Rodrigo Ferreira Rodrigues <dev.rodrigoferreira@gmail.com>
2021-07-04 21:02:55 -0300

    Parte 8 - Autenticacao de usuario e permissoes

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,12 +1,14 @@
 import uuid
 
+from datetime import date
+
 from django.db import models
 from django.db.models.aggregates import Max
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-
-
 class Genre(models.Model):
     """Model representing a book genre."""
     name = models.CharField(
@@ -77,13 +79,19 @@ class BookInstance(models.Model):
 
     status = models.CharField(max_length=1, choices=LOAN_STATUS,
                               blank=True, default='m', help_text='Book availability')
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String for representing the Model object"""
         return f'{self.id} ({self.book.title})'
+
+    @property
+    def is_overdue(self):
+        return self.due_back and date.today() > self.due_back
 
 
 class Author(models.Model):
